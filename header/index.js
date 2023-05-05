2ec8869e8a0528528d7ac042b99569dfdfe627df
BinhOP23 <glaubercastilho250@gmail.com>
2023-05-05 18:12:29 -0300

    first commit

---
+++ header/index.js
@@ -0,0 +1,20 @@
+let number = 42;
+let string = "text"
+let array = [43, 66,"Text", {chave: "Valor"}];
+Array 
+
+let obj = {
+    chave: "Valor",
+};
+
+let func = ()=>{
+    console.log("Eu sou um atributo de uma função")
+}
+let N1 = 12;
+let N2 = 45;
+let adição = N1+N2;
+let sub = N1-N2;
+let vezes= N1*N2;
+let dividir = N1/N2;
+let restoDaDivisão = N1%N2;
+console.log(N3)
